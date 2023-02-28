3cf4c02de64306279f1f0ac15db648befa2f2f02
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-02-28 14:56:28 +0000

    started foodItem controller

--- src/main/java/com/codeclan/MacrOS_backend/controllers/FoodItemController.java
+++ src/main/java/com/codeclan/MacrOS_backend/controllers/FoodItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class FoodItemController {
     @GetMapping(value = "/fooditems")
     public ResponseEntity<List<FoodItem>> getAllFoodItems(){
         return new ResponseEntity<>(foodItemRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/food/{id}")
+    public ResponseEntity getFood(@PathVariable Long id){
+        return new ResponseEntity<>(foodItemRepository.findById(id), HttpStatus.OK);
     }
 
 
