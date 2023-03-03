1bc26192e057cdf2948ad02d240511eaafa7fe13
Andrew Patterson <ajdpatterson@gmail.com>
2023-03-03 13:48:56 +0000

    added meals and days

--- src/main/java/com/codeclan/MacrOS_backend/components/DataLoader.java
+++ src/main/java/com/codeclan/MacrOS_backend/components/DataLoader.java
@@ -23,8 +23,8 @@ import java.util.List;
 import static java.time.temporal.TemporalQueries.localDate;
 
 
-//@Profile("!test")
-//@Component
+@Profile("!test")
+@Component
 public class DataLoader implements ApplicationRunner {
 
 
@@ -54,18 +54,52 @@ public class DataLoader implements ApplicationRunner {
 
         User user1 = new User("Brian", 29, "Male", 180, 80, 82, 84, ActivityLevel.ACTIVE, 1000L, 3000, 170, 120, 400, 80);
         userRepository.save(user1);
-        Day day1 = new Day(LocalDate.of(2023, 2, 27),user1);
+        Day day1 = new Day(LocalDate.of(2023, 3, 1),user1);
         Day day2 = new Day(LocalDate.of(2023, 2, 26), user1);
         Day day3 = new Day(LocalDate.of(2023, 2, 25), user1);
         Day day4 = new Day(LocalDate.of(2023, 2, 24), user1);
         Day day5 = new Day(LocalDate.of(2023, 2, 23), user1);
+        Meal meal1 = new Meal(MealType.BREAKFAST, day1);
+        Meal meal2 = new Meal(MealType.LUNCH, day1);
+        Meal meal3 = new Meal(MealType.DINNER, day1);
+        Meal meal4  = new Meal(MealType.SNACK, day1);
+        FoodItem foodItem1 = new FoodItem("Chips", 50, 100, 50, 100, 1000, "123456789");
+        FoodItem foodItem2 = new FoodItem("Cheese", 30, 100, 70, 200, 1000, "123456789");
+        FoodItem foodItem3 = new FoodItem("Curry Sauce", 100, 90, 60, 150, 1000, "123456789");
+        FoodItem foodItem4 = new FoodItem("Curry Sauce", 100, 90, 60, 150, 1000, "123456789");
+        foodItemRepository.save(foodItem1);
+        foodItemRepository.save(foodItem2);
+        foodItemRepository.save(foodItem3);
+        foodItemRepository.save(foodItem4);
+        List<FoodItem> breakfastFoodItemList;
+        List<FoodItem> lunchFoodItemList;
+        List<FoodItem> dinnerFoodItemList;
+        List<FoodItem> snackFoodItemList;
+        breakfastFoodItemList.add(foodItem1);
+        lunchFoodItemList.add(foodItem2);
+        dinnerFoodItemList.add(foodItem3);
+        snackFoodItemList.add(foodItem4);
+        meal1.setFoodItems(breakfastFoodItemList);
+        meal2.setFoodItems(lunchFoodItemList);
+        meal3.setFoodItems(dinnerFoodItemList);
+        meal4.setFoodItems(snackFoodItemList);
+        mealRepository.save(meal1);
+        mealRepository.save(meal2);
+        mealRepository.save(meal3);
+        mealRepository.save(meal4);
+        List<Meal> day1Meals = null;
+        day1Meals.add(meal1);
+        day1Meals.add(meal2);
+        day1Meals.add(meal3);
+        day1Meals.add(meal4);
+        day1.setMeals(day1Meals);
+        day2.setMeals(day1Meals);
+        day3.setMeals(day1Meals);
         dayRepository.save(day1);
         dayRepository.save(day2);
         dayRepository.save(day3);
         dayRepository.save(day4);
         dayRepository.save(day5);
-        Meal meal1 = new Meal(MealType.BREAKFAST, day1);
-        mealRepository.save(meal1);
 
 
     }
